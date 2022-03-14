e058e27be9714c2cf8b987118ee482be93a0475a
mingzeliu <lmzbcy1205@gmail.com>
2022-03-13 23:46:11 -0400

    Add Autocomplete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from "react";
 import { Map } from "./components/Map/Map.js";
 import { Permission } from "./components/Permission/Permission.js";
-import "./App.css";
+import s from "./App.css";
 import { useJsApiLoader } from "@react-google-maps/api";
-
+import { Autocomplete } from "./components/Autocomplete/Autocomplete.js";
 
 const defaultCenter = {
   lat: 43.6532,
@@ -12,17 +13,28 @@ const defaultCenter = {
 const libraries = ["places"];
 
 const App = () => {
+  const [center, setCenter] = React.useState(defaultCenter)
   const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
+    id: "google-map-script-1",
     googleMapsApiKey: "AIzaSyAeeg0Kwn0suOg-bC2hxzZpGiV2uMFV7gg",
     libraries,
   });
+
+  const onPlacesSelect = React.useCallback(
+    (Coordinates) => {
+      setCenter(Coordinates)
+    },
+    [],
+  )
+
   return (
     <div className="App">
-      {isLoaded ? <Map center={defaultCenter} /> : <h2>Loading</h2>},
+      <div className={s.addressSearchContainer}>
+        <Autocomplete isLoaded={isLoaded} onSelect={onPlacesSelect}/>
+      </div>
+      {isLoaded ? <Map center={center} /> : <h2>Loading</h2>},
       <Permission />
     </div>
-   
   );
 };
 
